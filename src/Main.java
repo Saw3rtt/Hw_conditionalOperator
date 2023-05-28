4bfac7080026ae67680d9986453c28764a30debc
Saw3rtt <sawertpra@gmail.com>
2023-05-28 15:23:34 +0300

    Задание 1-3

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args){
+        //task1
+        int age = 18;
+        if (age >=18){
+            System.out.println("Он совершеннолетний.");
+        }else{
+            System.out.println("он не достиг совершеннолетия, нужно немного подождать.");
+        }
+
+        //task2
+        int temperature = -1;
+        if(temperature < 5){
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        }else{
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+
+        }
+        //task3
+        int speed = 50;
+        if(speed > 60){
+            System.out.println("Если скорость, " + speed + " то придется заплатить штраф.");
+        }else{
+            System.out.println("Если скорость, " + speed + " то можно ездить спокойно");
+        }
+
+    }
+        }
+
